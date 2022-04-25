6788cbe3a30ae7da7ffbdeabbca243ef46db6be0
vdalex <9810260@gmail.com>
2022-04-25 22:12:27 +0300

    add VUE3 component example

---
+++ examples/Vue3Component/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container mt-3">
+    <h3 class="text-center">Simple Example RTSPtoWEB mediaplayer VUE.JS</h3>
+    <div class="row">
+      <div class="col-12">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Type url link here" ref="link">
+          <button class="btn btn-outline-secondary" type="button" @click="playVideo">PLAY</button>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            PLAYER
+          </div>
+          <div class="card-body p-0">
+            <PlayerVue :link='link'/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PlayerVue from "./components/PlayerVue.vue";
+export default {
+  name: 'App',
+  components: {
+    PlayerVue
+  },
+  data () {
+    return {
+      link: ''
+    }
+  },
+  methods:{
+    playVideo(){
+      this.link = this.$refs.link.value;
+    }
+  }
+}
+</script>
---
+++ examples/Vue3Component/src/components/PlayerVue.vue
@@ -0,0 +1,22 @@
+<template>
+  <div  ref="player" />
+</template>
+
+<script>
+import RTSPtoWEBPlayer from "rtsptowebplayer";
+export default {
+  name: "PlayerVue",
+  props: ['link'],
+  mounted: function (){
+    this.player=new RTSPtoWEBPlayer({
+      parentElement:this.$refs.player
+    });
+  },
+  updated: function (){
+    this.player.load(this.link);
+  },
+  beforeUnmount:function(){
+    this.player.destroy()
+  }
+}
+</script>
